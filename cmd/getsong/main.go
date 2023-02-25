031d126fac87a5b9925ede1c957183ab61e0cd38
William Comnisky <w.comnisky@gmail.com>
2023-02-25 14:03:29 +0100

    upgraded libs and a quick fix

--- cmd/getsong/main.go
+++ cmd/getsong/main.go
@@ -11,9 +11,7 @@ func main() {
 	if len(os.Args) != 3 {
 		fmt.Println(`Usage:
 		
-getsong 'title of song' 'artist of song'
-
-`)
+getsong 'title of song' 'artist of song'`)
 		os.Exit(1)
 	}
 	fname, err := getsong.GetSong(os.Args[1], os.Args[2], getsong.Options{
